6b257c53904434f9f03ec41450d7f84c31d22360
Ryan Calingo <gryan.calingo@gmail.com>
2018-02-02 23:29:26 -0500

    add read_DB func

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -11,3 +11,20 @@ var connection = mysql.createConnection({
     password: 'root',
     database: 'bamazon'
 });
+
+connection.connect(function(err) {
+    if (err) throw err;
+    initSetup();
+});
+function read_DB() {
+    connection.query("SELECT * FROM products", function(err, res) {
+        if (err) throw err;
+
+        console.log(res);
+    });
+}
+
+function initSetup() {
+    read_DB();
+      
+}
